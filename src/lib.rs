6626d939465b995527e088191eb4d9a5d70b7564
William Morgan <william.morgan@hotmail.co.uk>
2026-01-20 20:19:01 +0000

    /subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::{App, HttpResponse, HttpServer, dev::Server, web};
 use chrono::Utc;
+use serde::Deserialize;
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
@@ -11,6 +12,16 @@ async fn date() -> HttpResponse {
     HttpResponse::Ok().body(now)
 }
 
+#[derive(Deserialize)]
+struct Subsciption {
+    name: String,
+    email: String,
+}
+
+async fn subscriptions(_: web::Form<Subsciption>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
@@ -19,6 +30,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/date", web::get().to(date))
+            .route("/subscriptions", web::post().to(subscriptions))
     })
     .listen(listener)?
     .run();
